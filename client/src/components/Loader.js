1b222cf80b46a7cf8b0ac3f5fb8027a956930c12
Ivan Weber <ivanbdn@yandex.ru>
2021-02-06 11:52:45 +0300

    loader spinner style unificated

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Loader = () => (
     <div style={{display: 'flex', justifyContent: 'center', paddingTop: '2rem'}}>
       <div className="preloader-wrapper active">
-        <div className="spinner-layer spinner-red-only">
+        <div className="spinner-layer spinner-blue-only">
           <div className="circle-clipper left">
             <div className="circle" />
           </div><div className="gap-patch">
