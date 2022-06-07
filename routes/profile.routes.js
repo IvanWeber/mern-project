7465a19a8f9426c6516d27d27a801874b4affbbb
Ivan <ivanbdn@yandex.ru>
2022-06-08 01:42:50 +0300

    disable avatar rewriting

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -6,34 +6,34 @@ const router = Router()
 let fs = require('fs');
 
 
-router.get('/avatar/:id', auth, async (req, res) => {
-  try {
-    const avatarSrcUrl = __dirname + '/../uploads/avatars/' + 'user-avatar-' + req.params.id + '.jpg'
-    const avatarDistUrl = __dirname + '/../client/src/img/user-avatar.jpg'
+// router.get('/avatar/:id', auth, async (req, res) => {
+//   try {
+//     const avatarSrcUrl = __dirname + '/../uploads/avatars/' + 'user-avatar-' + req.params.id + '.jpg'
+//     const avatarDistUrl = __dirname + '/../client/src/img/user-avatar.jpg'
 
-    const avatarAnonymousSrcUrl = __dirname + '/../img/anonymous-avatar.jpg'
+//     const avatarAnonymousSrcUrl = __dirname + '/../img/anonymous-avatar.jpg'
 
-    if (fs.existsSync(avatarSrcUrl)) {
-      fs.copyFile(avatarSrcUrl, avatarDistUrl, (err) => {
-        if (err) throw err;
-        console.log('source.jpg was copied to destination.jpg');
-      });
+//     if (fs.existsSync(avatarSrcUrl)) {
+//       fs.copyFile(avatarSrcUrl, avatarDistUrl, (err) => {
+//         if (err) throw err;
+//         console.log('source.jpg was copied to destination.jpg');
+//       });
   
-      res.status(200).json({message: 'Аватар загружен'})
-    } else {
-      fs.copyFile(avatarAnonymousSrcUrl, avatarDistUrl, (err) => {
-        if (err) throw err;
-        console.log('source.jpg was copied to destination.jpg');
-      });
-      res.status(200).json({message: 'Аватар загружен'})
-    }
+//       res.status(200).json({message: 'Аватар загружен'})
+//     } else {
+//       fs.copyFile(avatarAnonymousSrcUrl, avatarDistUrl, (err) => {
+//         if (err) throw err;
+//         console.log('source.jpg was copied to destination.jpg');
+//       });
+//       res.status(200).json({message: 'Аватар загружен'})
+//     }
 
     
 
-  } catch (e) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-  }
-})
+//   } catch (e) {
+//     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+//   }
+// })
 
 router.get('/user', auth, async (req, res) => {
   try {
